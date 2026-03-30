6f53de9418200e6dbba646f7081b6c57288a57e6
rotmarer23 <167620937+Timyr2012@users.noreply.github.com>
2026-03-30 13:28:18 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        Random rand = new Random();
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            //Середній рівень завдання 19
+            int seconds1 = 7893;
+            int minutes = seconds1 / 60;
+            Console.WriteLine(minutes);
+            //Середній рівень завдання 20
+            int seconds2 = 8973;
+            int hours = seconds2 / 360;
+            Console.WriteLine(hours);
+            //Середній рівень завдання 21-22???
+            Console.WriteLine(seconds1 + seconds2);
+            //Середній рівень завдання 23 варіант 1
+            Console.WriteLine((seconds1 + seconds2)/60);
+            //Середній рівень завдання 23 варіант 2
+            Console.WriteLine(minutes);
+            //Середній рівень завдання 23 варіант 3
+            Console.WriteLine(hours/60);
+
+
+        }
+    }
+}
